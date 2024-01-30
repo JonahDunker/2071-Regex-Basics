778ea8d41ba020faae45a1947204699c96919aca
Jona Dunker <100035783@csisd>
2024-01-30 14:35:16 -0600

    eh

--- RegexBasics.java
+++ RegexBasics.java
@@ -7,11 +7,29 @@ import java.util.regex.Matcher;
 public class RegexBasics {
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
-    String hex = "^[a-f[A-F[0-9]]]{6}$";
-    String phone = "^[(]\\d{3}+[)]\\s\\d{3}[-]\\d{4}$"+
+    String[] regexes = new String[3];
+    String[] types = {"hex color code","phone number","email address"};
+    regexes[0] = "^[a-f[A-F[0-9]]]{6}$";
+    regexes[1] = "^[(]\\d{3}+[)]\\s\\d{3}[-]\\d{4}$"+
     "|^\\d{3}+[-]\\d{3}[-]\\d{4}$"+
     "|^\\d{10}$";
-    String email = "^[\\w[._-]]{1,}[@][\\w[-]]{1,}[.][a-zA-Z]{2,4}$";
-    System.out.println(in.nextLine().matches(email));
+    regexes[2] = "^[\\w[._-]]{1,}[@][\\w[-]]{1,}[.][a-zA-Z]{2,4}$";
+    ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
+    for(int i = 0; i < 3; i++) {
+      Scanner fin = new Scanner(new File(in.nextLine()));
+      arr.add(new ArrayList<String>());
+      while(fin.hasNextLine()) {
+        arr.get(i).add(fin.nextLine());
+      }
+    }
+    for(int i = 0; i < arr.size(); i++) {
+      for(int j = 0; j < arr.get(i).size(); j++) {
+        System.out.println(
+          arr.get(i).get(j)+" is"+
+          (arr.get(i).get(j).matches(regexes[i]) ? " not " : " ")+
+          "a valid "+types[i]
+        );
+      }
+    }
   }
 }
